128a13a23c0edbae1b524c7fd8a3a04b8e853b70
poshpotato <poshestpotato@gmail.com>
2021-03-04 13:22:02 +1300

    Added inital variables for the Slap21SimMain and Human classes

--- Human.java
+++ Human.java
@@ -17,8 +17,19 @@ public class Human
      *     Process state.
      *     Report infections/recoveries.
      */
-    public Human()
-    {
+    
+    //Human must store:
+    //The time left for them to infect or recover. 
+    int infectCount= 0;
+    int recoverCount = 0;
+    
+    //Their x and y positions relative to top-left.
+    int x = 0;
+    int y = 0;
+    
+    //The direction they are facing. Starting with 0=up and proceeding clockwise. 0=up,1=right,2=down,3=left. This is a byte to save memory.
+    byte direction = 0;
+    public Human(){
         
     }
    
--- Slap21SimMain.java
+++ Slap21SimMain.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Scanner;
 
 /**
  *
@@ -16,11 +18,30 @@ public class Slap21SimMain
      * Navigation between menus
      * Buildup and teardown of program
      */
+
+    //Slap21SimMain must store:
+    //The settings as a String, int hashmap.
+    static HashMap<String, Integer> settings = new HashMap<String, Integer>();
+    /*The settings stored and their default values are:
+     *Simulation x size: 100
+     *Simulation y size: 100
+     *Number of people: 30
+     *Number of initial infected: 1
+     *Chance of transmission: 100
+     *Infectious time: 10
+     *Recovery time: 10
+     *Simulation duration: Twice the x axis, if size is default then 200.
+     */
+    //A scanner for reading/writing to settings.txt
+    Scanner readWrite;
+    //A empty Simulation object to fill with the currently running simulation
+    Simulation currentSim;
+
     public Slap21SimMain()
     {
-        
+
     }
-    
+
     public int sampleMethod(int y)
     {
         // put your code here
