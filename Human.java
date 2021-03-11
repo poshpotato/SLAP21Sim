16ec420ea8ff430410b7e0ac71ec57e9f6ecc32f
poshpotato <poshestpotato@gmail.com>
2021-03-11 13:20:51 +1300

    Initial human movement method.

--- Human.java
+++ Human.java
@@ -26,6 +26,10 @@ public class Human
     //Their x and y positions relative to top-left.
     int x = 0;
     int y = 0;
+    
+    //The maximum x and y values of the current simulation.
+    int maxX;
+    int maxY;
 
     //The direction they are facing. Starting with 0=up and proceeding clockwise. 0=up,1=right,2=down,3=left. 
     //This used to be a byte to save memory, but problems with random generation forced a change to int.
@@ -34,10 +38,10 @@ public class Human
     //A human, when created, needs x, y, direction, and infection count set at minimum.
     
     //If all parameters specified. Generally for use in testing to ensure collisions.
-    public Human(int x, int y, byte direction, int infectCount){
+    public Human(int x, int y, int maxX, int maxY, int direction, int infectCount){
         this.x = x;
         this.y = y;
-        this.direction = direction;
+        this.direction = direction % 4;
         this.infectCount = infectCount;
         //printDebugStats();
     }
@@ -49,7 +53,7 @@ public class Human
         this.x = rand.nextInt(maxX);
         this.y = rand.nextInt(maxY);
         this.direction = rand.nextInt(4);
-        printDebugStats();
+        //printDebugStats();
     }
     
     //Random infected generation with parameters.
@@ -60,20 +64,45 @@ public class Human
         this.y = rand.nextInt(maxY);
         this.direction = rand.nextInt(4);
         this.infectCount = infectCount;
-        printDebugStats();
+        //printDebugStats();
     }
 
-    public int sampleMethod(int y)
-    {
-        // put your code here
-        return y;
+    public void move(){
+        //This will run every round.
+        //Check if by wall and change direction, check direction and adjust x and y values.
+        if(x == maxX-1 || y == maxY-1)direction = (direction+2)%4;
+        switch(direction){
+            case 0:
+                //north.
+                y -= 1;
+                break;
+            case 1:
+                //east.
+                x += 1;
+                break;
+            case 2:
+                //south.
+                y += 1;
+                
+                break;
+            case 3:
+                //west.
+                x -= 1;
+                break;
+            default:
+                //This is in case of an invalid direction. The person will not move.
+                System.out.println("Invalid direction: " + direction + ".");
+                break;
+        }
     }
     
+    
+    
     //A function to print statistics for a human. for testing purposes.
-    public void printDebugStats(){
+    /*public void printDebugStats(){
         System.out.println("x: " + x);
         System.out.println("y: " + y);
         System.out.println("direction: " + direction);
         System.out.println("infectCount: " + infectCount);
-    }
+    }*/
 }
